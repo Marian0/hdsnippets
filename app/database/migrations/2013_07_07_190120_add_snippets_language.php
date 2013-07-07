1f3f019a367d769a60766aa14a15cbaceda7f864
Germanaz0 <germanazo@gmail.com>
2013-07-07 16:05:57 -0300

    Add migrations, an extra field code_language will appear

---
+++ app/database/migrations/2013_07_07_190120_add_snippets_language.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSnippetsLanguage extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('snippets', function(Blueprint $table)
+		{
+			$table->integer('code_language')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('snippets', function(Blueprint $table)
+		{
+			$table->dropColumn('code_language');
+		});
+	}
+
+}
