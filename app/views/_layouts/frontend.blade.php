efc420c08d0b8c004593720e6888e661b6a33671
Germanaz0 <germanazo@gmail.com>
2013-07-07 14:52:28 -0300

    formatted

--- app/views/_layouts/frontend.blade.php
+++ app/views/_layouts/frontend.blade.php
@@ -1,22 +1,22 @@
 <!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>L4 Site</title>
+	<head>
+		<meta charset="utf-8">
+		<title>HDSnippets</title>
 
-	@include('_partials.assets')
-</head>
- <body class="preview" id="top" data-spy="scroll" data-target=".subnav" data-offset="80">
-	@include('_partials.navigation')
-  	 <div class="container">
-  	 		<div class="main">
+		@include('_partials.assets')
+	</head>
+	<body class="preview" id="top" data-spy="scroll" data-target=".subnav" data-offset="80">
+		@include('_partials.navigation')
+		<div class="container">
+			<div class="main">
 				@yield('main')
-  	 		</div>
+			</div>
 
-	@include('_partials.footer')
-     </div><!-- /container -->
-    <script src="http://bootswatch.com/js/jquery.smooth-scroll.min.js"></script>
-    <script src="http://bootswatch.com/js/bootstrap.min.js"></script>
-    <script src="http://bootswatch.com/js/bootswatch.js"></script>
-</body>
+			@include('_partials.footer')
+		</div><!-- /container -->
+		<script src="http://bootswatch.com/js/jquery.smooth-scroll.min.js"></script>
+		<script src="http://bootswatch.com/js/bootstrap.min.js"></script>
+		<script src="http://bootswatch.com/js/bootswatch.js"></script>
+	</body>
 </html>
